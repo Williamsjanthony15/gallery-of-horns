2edcad85d2d9f40deace87d98058c86695863487
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-03-24 23:10:38 -0500

    lab 02 good to go

--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import HornedBeast from './HornedBeast.js';
+import data from './data.json';
 
 class Main extends React.Component {
   render() {
-    return(
-   <main> 
-    <HornedBeast
-    title = 'UniWhal'
-    imageUrl = 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'
-    description = 'A unicorn and a narwhal nuzzling their horns' />
-  
-    <HornedBeast
-    title = 'UniWhal'
-    imageUrl = 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'
-    description = 'A unicorn and a narwhal nuzzling their horns' />
-   </main>
+    let beastArray = [];
+    data.forEach((animal) => {
+      beastArray.push (
+        <HornedBeast
+      name = { animal.keyword }
+      title = { animal.title }
+      image_url = { animal.image_url }
+      description = { animal.description }
+      />
+      );
+    })
+    return (
+      <div>
+        {beastArray};
+      </div>
     )
   }
 }
