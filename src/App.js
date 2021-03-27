a9c868f09fedb4184a2b13e665d06dc3f93cdf7f
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-03-27 18:38:28 -0500

    half done lab 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-// import data  from 'data.json';  <-----
+import data  from './data.json';
 
 import Header from './Header.js';
 import Main from './Main.js';
@@ -8,11 +8,36 @@ import Footer from './Footer.js';
 
 
 class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: 'false',
+      beast: {}
+    }
+    console.log('this is state -----', this.state);
+  }
+  
+  showBeastInModal = () => {
+    this.setState({
+      show: true,
+      beast: clickedBeast,
+    })
+  }
+  
+  // hideBeastInModal = ()
+  
+
   render(){
+    console.log(this.state);
     return (
+    
       <>
         <Header />
-        <Main />
+        <Main 
+          beasts = {data}
+          show = 
+        
+        />
         <Footer />
       </>
     );
