caaea838803c15b417bd38f3d7b3b7ac600fbe83
Anthony Williams <75553592+Williamsjanthony15@users.noreply.github.com>
2021-04-11 12:54:28 -0500

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HornedBeast from './HornedBeast.js';
 import CardColumns from 'react-bootstrap/CardColumns';
 import './Main.css';
+import { Form } from 'react-bootstrap';
 
 class Main extends React.Component {
   render() {
@@ -17,6 +18,17 @@ class Main extends React.Component {
     ));
     return (
       <>
+      <Form className="Form">
+        <Form.Group controlId="bootstrapName">
+          <Form.Label>Number of Horns</Form.label>
+            <Form.Control onInput = {this.props.handleFormSubmitted} as="select">
+             <option value="1">1 Horn</option>
+             <option value="2">2 Horn</option>
+             <option value="3">3 Horn</option>
+             <option value="100">100 Horn</option>
+            </Form.Control>
+        </Form.Group>
+      </form>
         <CardColumns>
           {beastArray};
       </CardColumns>
