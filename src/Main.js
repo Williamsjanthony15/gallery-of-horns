706838feb5c7ff413e40df1c2187a0b163f26c12
Anthony Williams <75553592+Williamsjanthony15@users.noreply.github.com>
2021-04-11 12:33:56 -0500

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import HornedBeast from './HornedBeast.js';
 import CardColumns from 'react-bootstrap/CardColumns';
-// import data from './data.json';
+import './Main.css';
 
 class Main extends React.Component {
   render() {
    let beastArray =  this.props.beasts.map((beast, index) => (
       <HornedBeast
-        name={beast.keyword}
-        title={beast.title}
-        image_url={beast.image_url}
-        description={beast.description}
+        name = {beast.keyword}
+        title = {beast.title}
+        image_url = {beast.image_url}
+        description = {beast.description}
         key = {index}
+        handleClick = {this.props.handleClick}
       />
     ));
     return (
