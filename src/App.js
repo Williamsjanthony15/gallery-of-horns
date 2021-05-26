f4d247a8e03e9377096d217164623a3e4c12f66f
Anthony Williams <Williamsjanthony15@yahoo.com>
2021-05-26 14:38:08 -0500

    Code Refactor for final submission

--- src/App.js
+++ src/App.js
@@ -1,45 +1,63 @@
 import React from 'react';
 import './App.css';
-import data  from './data.json';
-
+import data from './data.json';
+import SelectedBeast from './SelectedBeast';
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
 
 
-class App extends React.Component{
+
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      show: 'false',
+      show: false,
+      renderBeast: data,
       beast: {}
     }
     console.log('this is state -----', this.state);
   }
-  
-  showBeastInModal = () => {
+
+  showBeastInModal = (clickedBeast) => {
     this.setState({
       show: true,
+      renderBeast: data,
       beast: clickedBeast,
     })
   }
-  
-  // hideBeastInModal = ()
-  
 
-  render(){
+  hideBeastInModal = () => {
+    this.setState({
+      show: false,
+    })
+  }
+
+  handleFormSubmitted = event => {
+    event.preventDefault();
+    const horns = parseInt(event.target.value);
+    let filteredBeastSubmitted = data.filter(beasts => beasts.horns === horns);
+    this.setState({renderBeast: filteredBeastSubmitted})
+  }
+
+
+  render() {
     console.log(this.state);
     return (
-    
-      <>
+      <div>
         <Header />
         <Main 
-          beasts = {data}
-          show = 
-        
+          beasts = {this.state.renderBeast}
+          handleClick = {this.showBeastInModal}
+          handleFormSubmitted={this.handleFormSubmitted}
         />
+        <SelectedBeast
+          show={this.state.show}
+          hideBeast={this.hideBeastInModal}
+          beast={this.state.beast}
+          />
         <Footer />
-      </>
+      </div>
     );
   }
 }
--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HornedBeast from './HornedBeast.js';
 import CardColumns from 'react-bootstrap/CardColumns';
+import { Form } from 'react-bootstrap';
 // import data from './data.json';
 
 class Main extends React.Component {
@@ -12,14 +13,27 @@ class Main extends React.Component {
         image_url={beast.image_url}
         description={beast.description}
         key = {index}
+        handleClick={this.props.handleClick}
       />
     ));
+
     return (
-      <>
+      <main>
+        <Form className="Form">
+          <Form.Group controlId="BootStrapName">
+            <Form.Label>Number of Horns</Form.Label>
+            <Form.Control onInput={this.props.handleFormSubmitted} as="select">
+              <option value="1">1 horn</option>
+              <option value="2">2 Horns</option>
+              <option value="3">3 Horns</option>
+              <option value="100">100 Horns</option>
+            </Form.Control>
+          </Form.Group>
+        </Form>
         <CardColumns>
-          {beastArray};
-      </CardColumns>
-      </>
+          {beastArray}
+        </CardColumns>
+      </main>
     )
   }
 }
