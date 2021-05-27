58c3b0d737b390965d72e0ff81d7fff683b01c09
Anthony Williams <Williamsjanthony15@yahoo.com>
2021-05-27 13:07:59 -0500

    verification of compile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import './App.css';
-// import data  from 'data.json';  <-----
-
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
-
 
 class App extends React.Component{
   render(){
